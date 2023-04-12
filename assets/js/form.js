a965c75608e9affe2dfb754694d22adef2f5cd2a
Abdoulaye-MAcOS <njigouhrazak@iut-dhaka.edu>
2023-04-12 17:11:12 +0100

    feat: contact us page email

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,14 +5,17 @@
  * License: https://bootstrapmade.com/license/
  */
  const submitForm = (name, email, subject, message) => {
-     console.log("abdoulaye");
+    //  console.log(name);
+    //  console.log(email);
+    //  console.log(subject);
+    //  console.log(message);
 
     // POST request using fetch()
     fetch("https://18bsbinhgi.execute-api.us-east-1.amazonaws.com/develop/", {
 
             // Adding method type
             method: "POST",
-            mode: 'cors',
+            mode: 'no-cors',
 
             // Adding body or contents to send
             body: JSON.stringify({
@@ -35,16 +38,17 @@ const validateinputs = (event) => {
     event.preventDefault();
     let elements = document.getElementsByClassName('form-control2');
     let submit = true;
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
+    const name = document.getElementById("subject1").value;
+    const email = document.getElementById("subject2").value;
     const subject = document.getElementById("subject").value;
     const message = document.getElementById("message").value;
+  
+ 
+
 
 
     for (let i = 0, im = elements.length; im > i; i++) {
-        console.log(elements[i].value);
-        if (elements[i].value == "") {
+        if (elements[i].value === "") {
             // document.getElementById("error").style.display = "block";
             elements[i].style.border = "1px solid red";
             submit = false;
@@ -53,5 +57,6 @@ const validateinputs = (event) => {
     if (submit) {
         submitForm(name, email, subject, message);
     }
+
 
 } 
